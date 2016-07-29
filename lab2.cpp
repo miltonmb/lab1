2d30ebbaf3a7f7979870562271d26dfafa938dc2
miltonmb <milton.pasos@gmail.com>
2016-07-29 15:48:41 -0600

    Create new file

---
+++ lab2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+#include <stdio.h>
+
+long factorial(long);
+long metodo(long);
+
+int main (){
+	int op = 0;
+	char r = 'S';
+	while(r == 's'|| r == 'S'){
+		std::cout <<"Que ejercicio desea hacer: "<< "\n1.Ejercicio 1\n" << "2.Ejercicio 2\n";
+		std::cin >> op;
+		switch(op){
+			case 1:
+			long x = 1;
+			while(x <= 100000){
+				if(x == metodo(x)){
+					std::cout<<"El numero: "<<x<<" es correcto!\n";
+					++x;
+				}else{
+					++x;
+				}
+			}
+			break;
+
+			case 2:
+			
+				break;
+
+			}
+		}
+	}
+	long metodo(long x){
+		long res = 0;
+		long suma = 0;
+		while(x>0){
+			res = x%10;
+			x = x/10;
+			suma += factorial(res);
+		}
+		return suma;
+	}
+
+	long factorial(long s){
+		if(s==1 || s == 0){
+			return 1;
+		}else{
+			return s * factorial(s-1);
+		}
+	}
